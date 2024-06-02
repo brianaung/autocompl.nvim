a974f0cc89dbc4b499fe19fb8a34ff162495d90e
brianaung <brianaung16@gmail.com>
2024-06-02 16:43:39 +1000

    feat: integrate luasnip for snippet expansion

--- README.md
+++ README.md
@@ -2,20 +2,24 @@
 
 ### Goal of this project (in order of importance)
 To support:
-- [ ] Automatic code completion.
-- [ ] Different completion sources:
-    - [ ] Native LSP.
-    - [ ] Current buffer text (as fallback).
+- [x] Automatic code completion.
+- [x] Different completion sources:
+    - [x] Native LSP.
+    - [x] Current buffer text (as fallback).
+- [x] Snippet expansion.
 - [ ] Info preview window.
+
+#### Bug fixes
+- [ ] Keep completion suggestion window when deleting text
 
 ### Future roadmap
 - [ ] Fuzzy search.
-- [ ] Snippet expansion.
 
-### Non-Goals
+### Maybe(s)
 
 #### Custom keybindings support
-Most people should use Vim's "idiomatic" completion bindings (See `:h ins-completion`) since they are superior to the popular `<Tab>` and/or `<Enter>` completions bindings in my opinion.
+I will be focusing on other features since this is more for personal use and I don't need the ability to set custom keybindings.
+And most people should also just use Vim's "idiomatic" completion bindings (See `:h ins-completion`) since they are superior to the popular `<Tab>` and/or `<Enter>` completions bindings in my opinion.
 
 ### ~Similar~ Better alternatives
 - [nvim-cmp](https://github.com/hrsh7th/nvim-cmp)
--- lua/autocompl/init.lua
+++ lua/autocompl/init.lua
@@ -1,20 +1,55 @@
 local M = {}
 
-local compl_keys = {
+M.states = {
+  confirm_done = false,
+}
+
+M.keys = {
   completefunc = vim.keycode "<C-x><C-u>",
   fallback = vim.keycode "<C-x><C-n>",
 }
 
 M.setup = function()
+  local success, ls = pcall(require, "luasnip")
+
+  if not success then
+    vim.notify(
+      "(autocompl.nvim) Requires luasnip as a dependency to support snippet expansion."
+        .. " Please check your dependencies list.",
+      vim.log.levels.ERROR
+    )
+    return
+  end
+
   -- Set options
   vim.opt.completeopt = { "menuone", "popup", "noinsert" }
   vim.opt.shortmess:append "c"
+
+  -- Listen to confirm key pressed event
+  vim.on_key(function(key)
+    if vim.fn.pumvisible() == 1 and vim.keycode "<C-y>" == key then
+      M.states.confirm_done = true
+    end
+  end, 0)
 
   -- Set keybindings
   vim.keymap.set("i", "<CR>", function()
     return vim.fn.pumvisible() == 1 and vim.keycode "<C-e><CR>" or vim.keycode "<CR>"
   end, { expr = true })
 
+  -- LS Keybindings
+  vim.keymap.set({ "i", "s" }, "<C-l>", function()
+    if ls.expand_or_locally_jumpable() then
+      ls.expand_or_jump()
+    end
+  end, { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-h>", function()
+    if ls.locally_jumpable(-1) then
+      ls.jump(-1)
+    end
+  end, { silent = true })
+
+  -- Start
   M.create_autocmds()
 end
 
@@ -26,23 +61,7 @@ M.create_autocmds = function()
   -- Define autocommands
   au("LspAttach", M.set_completefunc, "Update completefunc if LSP client is available")
   au("InsertCharPre", M.trigger_completion, "Auto trigger completion")
-  au("CompleteDonePre", M.on_completion, "Process completed item")
-end
-
-M.trigger_completion = function()
-  if vim.fn.pumvisible() == 1 or vim.fn.state "m" == "m" then
-    return
-  end
-
-  if vim.api.nvim_buf_get_option(0, "buftype") == "" then
-    local key = vim.api.nvim_get_option_value("completefunc", {}) == "" and compl_keys["fallback"]
-      or compl_keys["completefunc"]
-    vim.api.nvim_feedkeys(key, "m", false)
-  end
-end
-
-M.on_completion = function()
-  -- vim.print(vim.v.completed_item)
+  au("CompleteDonePre", M.expand_snippet, "Expand snippet completion if confirm_done")
 end
 
 M.set_completefunc = function(e)
@@ -54,6 +73,41 @@ M.set_completefunc = function(e)
   else
     vim.bo[bufnr].completefunc = ""
   end
+end
+
+M.trigger_completion = function()
+  if vim.fn.pumvisible() == 1 or vim.fn.state "m" == "m" then
+    return
+  end
+
+  if vim.api.nvim_buf_get_option(0, "buftype") == "" then
+    local key = vim.api.nvim_get_option_value("completefunc", {}) == "" and M.keys["fallback"] or M.keys["completefunc"]
+    vim.api.nvim_feedkeys(key, "m", false)
+  end
+end
+
+M.expand_snippet = function()
+  if not M.states.confirm_done then
+    return
+  end
+
+  local completed_item = vim.v.completed_item
+  local completion_item = vim.tbl_get(completed_item, "user_data", "nvim", "lsp", "completion_item")
+
+  -- check that this is an lsp completion and is a snippet
+  if not (completion_item and completed_item.kind == "Snippet") then
+    return
+  end
+
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  vim.api.nvim_buf_set_text(0, row - 1, col - #completed_item.word, row - 1, col, { "" })
+  vim.api.nvim_win_set_cursor(0, { row, col - vim.fn.strwidth(completed_item.word) })
+
+  -- expand snippet
+  require("luasnip").lsp_expand(vim.tbl_get(completion_item, "textEdit", "newText") or completion_item.insertText or "")
+
+  M.states.confirm_done = false
 end
 
 return M

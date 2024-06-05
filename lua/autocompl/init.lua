231824b1237aa5fafb9273803f0ab12f2791d7c1
brianaung <brianaung16@gmail.com>
2024-06-06 02:04:51 +1000

    feat: add simple snippet support using vim.snippet.expand()

--- lua/autocompl/init.lua
+++ lua/autocompl/init.lua
@@ -17,6 +17,21 @@ M.setup = function()
   -- Define autocommands
   util.au("LspAttach", completion.set_completefunc, "Set completefunc if LSP client is available")
   util.au("InsertCharPre", completion.trigger_completion, "Start auto completion with a debounce")
+  util.au("CompleteDonePre", function()
+    local completion_item = vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp", "completion_item") or {}
+
+    -- do nth it's not a lsp completion or not a snippet
+    if vim.tbl_isempty(completion_item) or completion_item.kind ~= 15 then
+      return
+    end
+
+    local row = vim.api.nvim_win_get_cursor(0)[1]
+    local col = vim.api.nvim_win_get_cursor(0)[2]
+    vim.api.nvim_buf_set_text(0, row - 1, col - #vim.v.completed_item.word, row - 1, col, { "" })
+    vim.api.nvim_win_set_cursor(0, { row, col - vim.fn.strwidth(vim.v.completed_item.word) })
+
+    vim.snippet.expand(vim.tbl_get(completion_item, "textEdit", "newText") or completion_item.insertText or "")
+  end, "Start auto completion with a debounce")
 end
 
 return M

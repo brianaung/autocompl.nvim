27f4180fc1b04859aef3d216d2423f3e8a16f342
brianaung <brianaung16@gmail.com>
2024-06-06 23:40:24 +1000

    feat: apply additional text edits on completedonepre (wip)

--- lua/autocompl/init.lua
+++ lua/autocompl/init.lua
@@ -4,6 +4,8 @@ local util = require "autocompl.util"
 
 local M = {}
 
+M.ns_id = vim.api.nvim_create_namespace "AutoCompl"
+
 M.setup = function()
   vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
   vim.opt.shortmess:append "c"
@@ -31,6 +33,50 @@ M.setup = function()
     vim.api.nvim_win_set_cursor(0, { row, col - vim.fn.strwidth(vim.v.completed_item.word) })
 
     vim.snippet.expand(vim.tbl_get(completion_item, "textEdit", "newText") or completion_item.insertText or "")
+  end, "Start auto completion with a debounce")
+
+  util.au("CompleteDonePre", function()
+    local lsp_data = vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp")
+    if lsp_data == nil or vim.tbl_isempty(lsp_data) then
+      return
+    end
+
+    local completion_item = lsp_data.completion_item
+
+    if completion_item then
+      local edits = completion_item.additionalTextEdits
+      local client_id = lsp_data.client_id
+      if edits then
+        local cur_pos = vim.api.nvim_win_get_cursor(0)
+        local extmark_id = vim.api.nvim_buf_set_extmark(0, M.ns_id, cur_pos[1] - 1, cur_pos[2], {})
+
+        local offset_encoding = vim.lsp.get_client_by_id(client_id).offset_encoding
+        vim.lsp.util.apply_text_edits(edits, vim.api.nvim_get_current_buf(), offset_encoding)
+
+        local extmark_data = vim.api.nvim_buf_get_extmark_by_id(0, M.ns_id, extmark_id, {})
+        pcall(vim.api.nvim_buf_del_extmark, 0, M.ns_id, extmark_id)
+        pcall(vim.api.nvim_win_set_cursor, 0, { extmark_data[1] + 1, extmark_data[2] })
+      end
+      -- vim.lsp.buf_request(0, "completionItem/resolve", completion_item, function(_, _, result)
+      --   local edits = vim.tbl_get(result, "params", "additionalTextEdits") or completion_item.additionalTextEdits
+      --   local client_id = result.client_id
+      --   if edits then
+      --     client_id = lsp_data.client_id
+      --   end
+
+      --   if edits then
+      --     local cur_pos = vim.api.nvim_win_get_cursor(0)
+      --     local extmark_id = vim.api.nvim_buf_set_extmark(0, M.ns_id, cur_pos[1] - 1, cur_pos[2], {})
+
+      --     local offset_encoding = vim.lsp.get_client_by_id(client_id).offset_encoding
+      --     vim.lsp.util.apply_text_edits(edits, vim.api.nvim_get_current_buf(), offset_encoding)
+
+      --     local extmark_data = vim.api.nvim_buf_get_extmark_by_id(0, M.ns_id, extmark_id, {})
+      --     pcall(vim.api.nvim_buf_del_extmark, 0, M.ns_id, extmark_id)
+      --     pcall(vim.api.nvim_win_set_cursor, 0, { extmark_data[1] + 1, extmark_data[2] })
+      --   end
+      -- end)
+    end
   end, "Start auto completion with a debounce")
 end
 
